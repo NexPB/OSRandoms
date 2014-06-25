a19472ff0ae95c7bbf40e94211561b2170dc70b3
Logicail <logicail@mike-nightingale.co.uk>
2014-06-25 19:18:38 +0100

    Also log status

--- src/pb/osrandoms/core/OSRandom.java
+++ src/pb/osrandoms/core/OSRandom.java
@@ -4,20 +4,23 @@ import java.util.logging.Logger;
 
 public abstract class OSRandom extends GraphScript.Action<RandomContext> {
 
-    public final Logger log;
+	public final Logger log;
 
-    private String status = "";
+	private String status = "";
 
-    public OSRandom(RandomContext ctx) {
-        super(ctx);
-        log = ctx.controller.script().log;
-    }
+	public OSRandom(RandomContext ctx) {
+		super(ctx);
+		log = ctx.controller.script().log;
+	}
 
-    public String status() {
-        return status;
-    }
+	public String status() {
+		return status;
+	}
 
-    public void status(String status) {
-        this.status = status == null ? "" : status;
-    }
+	public void status(String status) {
+		if (status != null) {
+			log.info(status);
+		}
+		this.status = status == null ? "" : status;
+	}
 }
