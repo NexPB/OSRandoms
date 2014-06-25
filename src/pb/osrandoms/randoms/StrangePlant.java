139674116aeed33976806bfd3cbc6b0fed201da0
Logicail <logicail@mike-nightingale.co.uk>
2014-06-25 19:50:39 +0100

    StrangePlant status and target

--- src/pb/osrandoms/randoms/StrangePlant.java
+++ src/pb/osrandoms/randoms/StrangePlant.java
@@ -14,42 +14,43 @@ import java.util.concurrent.Callable;
  * - Make sure false animations are right
  */
 public class StrangePlant extends OSRandom {
+	private final int[] FALSE_ANIMATIONS = {348, 350};
+	private final String PLANT_NAME = "Strange plant";
 
-    private final int[] FALSE_ANIMATIONS = {348, 350};
-    private final String PLANT_NAME = "Strange plant";
+	public StrangePlant(RandomContext ctx) {
+		super(ctx);
+	}
 
-    public StrangePlant(RandomContext ctx) {
-        super(ctx);
-    }
+	@Override
+	public void run() {
+		final Npc plant = getPlant();
+		status("Pick strange plant.");
+		target.set(plant);
+		if (plant.inViewport() && plant.interact("Pick", plant.name())) {
+			Condition.wait(new Callable<Boolean>() {
+				@Override
+				public Boolean call() throws Exception {
+					return !getPlant().valid();
+				}
+			}, 250, 10);
+		} else {
+			status("Walk to strange plant.");
+			ctx.movement.step(plant);
+		}
+	}
 
-    public Npc getPlant() {
-        return ctx.randomMethods.getNpc(new Filter<Npc>() {
-            @Override
-            public boolean accept(Npc npc) {
-                return Arrays.binarySearch(FALSE_ANIMATIONS, npc.animation()) > -1 &&
-                        npc.interacting().equals(ctx.players.local());
-            }
-        }, PLANT_NAME);
-    }
+	public Npc getPlant() {
+		return ctx.randomMethods.getNpc(new Filter<Npc>() {
+			@Override
+			public boolean accept(Npc npc) {
+				return Arrays.binarySearch(FALSE_ANIMATIONS, npc.animation()) > -1 &&
+						npc.interacting().equals(ctx.players.local());
+			}
+		}, PLANT_NAME);
+	}
 
-    @Override
-    public boolean valid() {
-        return getPlant().valid();
-    }
-
-    @Override
-    public void run() {
-        final Npc plant = getPlant();
-        if (plant.inViewport() && plant.interact("Pick", plant.name())) {
-            Condition.wait(new Callable<Boolean>() {
-                @Override
-                public Boolean call() throws Exception {
-                    return !getPlant().valid();
-                }
-            }, 250, 10);
-        } else {
-            ctx.movement.step(plant);
-        }
-    }
-
+	@Override
+	public boolean valid() {
+		return getPlant().valid();
+	}
 }
