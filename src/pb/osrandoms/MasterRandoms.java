c0ea60ad1cae5d694a45d39d08354abbd336b2ea
Nex <nex.powerbot@hotmail.com>
2014-06-29 00:43:44 +0200

    Added Quiz Master

--- src/pb/osrandoms/MasterRandoms.java
+++ src/pb/osrandoms/MasterRandoms.java
@@ -11,25 +11,25 @@ import java.awt.*;
 import java.util.Arrays;
 
 @Script.Manifest(
-		name = "Randoms",
-		description = "Sloves randoms to bot 24/7.",
-		properties = "topic=-1;client=4"
+        name = "Randoms",
+        description = "Sloves randoms to bot 24/7.",
+        properties = "topic=-1;client=4"
 )
 public class MasterRandoms extends GraphScript<RandomContext> implements PaintListener {
 
-	public MasterRandoms() {
-		chain.addAll(Arrays.asList(new StrangePlant(ctx), new ExpRewardClaimer(ctx), new TalkToRandoms(ctx), new Pinball(ctx), new SandwichLady(ctx)));
-	}
+    public MasterRandoms() {
+        chain.addAll(Arrays.asList(new QuizMaster(ctx), new StrangePlant(ctx), new ExpRewardClaimer(ctx), new TalkToRandoms(ctx), new Pinball(ctx), new SandwichLady(ctx), new Mime(ctx)));
+    }
 
-	@Override
-	public void repaint(Graphics g) {
-		g.setFont(new Font("Tahoma", 0, 14));
-		final OSRandom action = (OSRandom) this.current.get();
-		if (action != null) {
-			action.repaint(g);
-			g.setColor(Color.WHITE);
-			g.drawString(action.status(), 10, 50);
-		}
-	}
+    @Override
+    public void repaint(Graphics g) {
+        g.setFont(new Font("Tahoma", 0, 14));
+        final OSRandom action = (OSRandom) this.current.get();
+        if (action != null) {
+            action.repaint(g);
+            g.setColor(Color.WHITE);
+            g.drawString(action.status(), 10, 50);
+        }
+    }
 
 }
---
+++ src/pb/osrandoms/randoms/QuizMaster.java
@@ -0,0 +1,73 @@
+package pb.osrandoms.randoms;
+
+import org.powerbot.script.Condition;
+import org.powerbot.script.rt4.Component;
+import org.powerbot.script.rt4.Widget;
+import pb.osrandoms.core.OSRandom;
+import pb.osrandoms.core.RandomContext;
+
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
+public class QuizMaster extends OSRandom {
+
+    private final int[][] ANSWERS = {
+            {6190, 6189},
+            {6198, 6197},
+            {6191, 6192},
+            {6193, 6194},
+            {6195, 6196}
+    };
+
+    public QuizMaster(RandomContext ctx) {
+        super(ctx);
+    }
+
+    private Widget getWidget() {
+        return ctx.randomMethods.getComponentByText("Pick the odd one out.").widget();
+    }
+
+    private Component getAnswer() {
+        final Component[] components = getWidget().components();
+        if (components.length > 0) {
+            for (int[] arr : ANSWERS) {
+                for (Component component : components) {
+                    if (Arrays.binarySearch(arr, component.modelId()) > -1)
+                        return component;
+                }
+            }
+        }
+        return ctx.widgets.widget(0).component(0);
+    }
+
+    @Override
+    public boolean valid() {
+        return ctx.npcs.select().name("Quiz Master").nearest().poll().valid();
+    }
+
+    @Override
+    public void run() {
+        final Component prize = ctx.randomMethods.getComponentByText("1000 Coins");
+        if (prize.valid()) {
+            status("Select our prize: 1000 Coins.");
+            target.set(prize);
+            if (prize.click()) {
+                Condition.wait(new Callable<Boolean>() {
+                    @Override
+                    public Boolean call() throws Exception {
+                        return !ctx.npcs.select().name("Quiz Master").nearest().poll().valid();
+                    }
+                }, 200, 6);
+            }
+        } else {
+            final Component answer = getAnswer();
+            if (answer.valid()) {
+                status("Clicking answer.");
+                target.set(answer);
+                if (answer.click())
+                    Condition.sleep(1500);
+            }
+        }
+    }
+
+}
